3b2aa65c8622cd4bf1af290444aabcc6830af7c5
Alan <alanrodrigo.mg@gmail.com>
2024-09-28 00:01:40 -0400

    Subiendo ultimos cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             RoleHasPermissionSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
